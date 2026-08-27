3d9950debc07c5cad17379aad9c10773b7561419
Eduardo0308 <eduardodudu0310@gmail.com>
2026-08-26 21:23:54 -0300

    Atualizar o bot.py

--- Bot_Écho/bot.py
+++ Bot_Écho/bot.py
@@ -7,6 +7,7 @@ from comandos import get_class
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
+from deep_translator import GoogleTranslator
 
 
 
